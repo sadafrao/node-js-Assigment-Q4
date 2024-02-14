efcd51a74fd7aaf1b1e6c66a8d7e3a75fe0bcfd4
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 13:10:27 -0800

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,5 @@
+"use strict";
+// Q4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+let name3 = ("Hazrat Ali said");
+console.log(`${name3}“ Each person's Grief is According To his courage”
+ `);
---
+++ Q4.ts
@@ -0,0 +1,6 @@
+// Q4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+
+let name3: string = ("Hazrat Ali said")
+
+ console.log(`${name3}“ Each person's Grief is According To his courage"`)
